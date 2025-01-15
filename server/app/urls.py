9b9813e9337b1939e21464a47e531e35ef3fbe2e
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-01-15 12:36:20 -0300

    fix: Alterado endpoint da view RegisterUserView de api/v1/login para api/v1/signup, pois o endpoint não se trata de um login, mas sim de um cadastro.

--- server/app/urls.py
+++ server/app/urls.py
@@ -4,6 +4,6 @@ from .views import HealthAPIView, RegisterUserView, ItemAPIView
 
 urlpatterns = [
     path('', HealthAPIView.as_view()),
-    path('api/v1/login', RegisterUserView.as_view()),
+    path('api/v1/signup', RegisterUserView.as_view()),
     path('api/v1/item/', ItemAPIView.as_view())
 ]
