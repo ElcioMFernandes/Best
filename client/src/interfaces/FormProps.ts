f36594ea2c89f8555e93fa0a1b059f4ccc7a5001
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-01-17 13:58:22 -0300

    feat: Update FormProps interface to accept data parameter in formAction

--- client/src/interfaces/FormProps.ts
+++ client/src/interfaces/FormProps.ts
@@ -1,6 +1,6 @@
 export interface FormProps {
   formTitle: string;
-  formAction: React.FormEvent<HTMLFormElement> | (() => void);
+  formAction: React.FormEvent<HTMLFormElement> | ((data: any) => void);
   formMethod: "GET" | "POST" | "PUT" | "DELETE";
   children: React.ReactNode;
 }
