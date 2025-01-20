d93a2ff589701d4b8f776dd196ebf2aa7804b967
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-01-20 14:06:12 -0300

    feat: Implement home page and enhance authentication flow with error handling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,18 +4,24 @@ import { Frame } from "@/components/Frame";
 import { Input } from "@/components/Input";
 import { Form } from "@/components/Form";
 import { fetchAuth } from "@/services/auth";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleLogin = async (data: any) => {
+    const success = await fetchAuth(data);
+    if (success) {
+      router.push("/home");
+    } else {
+      alert("Erro ao autenticar. Verifique suas credenciais.");
+    }
+  };
+
   return (
     <>
       <Frame displayNavBar={false} displayFooter={false}>
-        <Form
-          formTitle="Entrar"
-          formAction={(data: any) => {
-            fetchAuth(data);
-          }}
-          formMethod="POST"
-        >
+        <Form formTitle="Entrar" formAction={handleLogin} formMethod="POST">
           <Input
             id="matricula-input"
             type="text"
--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,9 +1,23 @@
 import axios from "axios";
 
 export const fetchAuth = async (data: any) => {
-  const response = await axios.post(
-    "http://127.0.0.1:8000/api/v1/auth/token/",
-    data
-  );
-  console.log(response.data);
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/api/v1/auth/token/",
+      data
+    );
+
+    console.log(response.data);
+
+    if (response.data.detail) {
+      return false;
+    }
+    const { access, refresh } = response.data;
+    sessionStorage.setItem("accessToken", access);
+    sessionStorage.setItem("refreshToken", refresh);
+    return true;
+  } catch (error) {
+    console.log(`Erro ao autenticar: ${error}`);
+    return false;
+  }
 };
