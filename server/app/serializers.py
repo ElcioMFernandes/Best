99a75cc19eae13b3aaa07afceecbb8d7f7df45f0
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-01-21 13:59:44 -0300

    feat: Enhance CustomUserSerializer to include password validation and creation logic

--- server/app/serializers.py
+++ server/app/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser, Wallet, Product, Order, Transaction
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name']
+        fields = ['id', 'username', 'first_name', 'last_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        return user
 
 class WalletSerializer(serializers.ModelSerializer):
     user = CustomUserSerializer(read_only=True)
