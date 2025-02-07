52eb037ada7b97a7bede112ee73f1037d676c357
Elcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-02-07 13:07:12 -0300

    feat: Update Card component styles for improved layout and spacing

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -9,10 +9,10 @@ const Card = (props: CardProps) => {
         <img
           src={props.image}
           alt={props.title}
-          className="h-44 w-full p-4 rounded-xl shadow-lg dark:shadow-neutral-950/50"
+          className="h-44 w-full p-4 rounded-xl shadow-lg shadow-neutral-500/50 dark:shadow-neutral-950/50"
         />
         <p className="w-full text-xl">{props.title}</p>
-        <div className="w-full flex items-center gap-1">
+        <div className="w-full flex items-center gap-2">
           <SvgCoin />
           <p className="">{props.subtitle}</p>
         </div>
